2176f2e4c80cc5033121977f6d33ea6b5fa47bcb
kenny <k.mackormic@mail.ru>
2024-04-24 16:19:01 +0300

    change back logo in  manifest and siteMetadata fix page about size-text

--- src/components/About/AboutCoverSection.js
+++ src/components/About/AboutCoverSection.js
@@ -5,7 +5,7 @@ import profileCharacter from "@/public/me-bg.png";
 
 const AboutCoverSection = () => {
   return (
-    <section className="w-full md:h-[75vh] border-b-2 border-solid border-dark dark:border-light flex flex-col md:flex-row items-center justify-center text-dark dark:text-light">
+    <section className=" w-full md:h-[75vh] border-b-2 border-solid border-dark dark:border-light flex flex-col md:flex-row items-center justify-center text-dark dark:text-light">
       <div className=" w-full md:w-1/2 h-full border-r-2 border-solid border-dark dark:border-light flex justify-center">
         <Image
           src={profileCharacter}
@@ -16,13 +16,14 @@ const AboutCoverSection = () => {
         />
       </div>
 
-      <div className="w-full md:w-1/2 flex flex-col text-left items-start justify-center px-5 xs:p-10 pb-10 lg:px-16">
-        <h2 className="font-bold  text-4xl xs:text-5xl sxl:text-6xl  text-center lg:text-left">
+      <div className=" w-full md:w-1/2 flex flex-col text-left items-start justify-center px-5 xs:p-10 pb-10 lg:px-16">
+        <h2 className="  font-bold  text-3xl xs:text-4xl sxl:text-5xl  text-center lg:text-left">
           Мечтайте о многом, работайте усердно, достигайте большего!
         </h2>
-        <p className="font-medium  mt-4 text-base">
+        <p className="font-medium  mt-4 text-base md:text-sm">
           Окончил в 2012 году юридический факультет БГУ по специальности
-          «Правоведение». <br />
+          «Правоведение».
+          <br />
           <br />В 2011 году обучался в Lund University в Швеции. Магистр
           гендерных наук.
           <br />
