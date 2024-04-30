3d2a12aad28cd12d39acd24a0252b52b559ee3a7
kenny <k.mackormic@mail.ru>
2024-04-30 13:54:34 +0300

    change about Skills

--- src/components/About/Skills.js
+++ src/components/About/Skills.js
@@ -20,7 +20,7 @@ const Skills = () => {
     text-dark dark:text-light"
     >
       <span className="font-semibold text-lg sm:text-3xl md:text-4xl text-accent dark:text-accentDark">
-        Консультирую в сфере ...
+        Актуальные темы ...
       </span>
       <ul className="flex flex-wrap mt-8 justify-center  xs:justify-start">
         {SkillList.map((skill, index) => {
